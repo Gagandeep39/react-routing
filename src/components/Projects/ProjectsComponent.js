2275abe685a57cb966a9300d4a8614d41e025b8f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 17:19:21 +0530

    Added Project and Service component

---
+++ src/components/Projects/ProjectsComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class ProjectsComponent extends Component {
+  render() {
+    return (
+      <h1>You chose <span class="badge badge-primary">Projects</span></h1>
+    );
+  }
+}
+
+export default ProjectsComponent;
---
+++ src/components/Services/ServicesComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class ServicesComponent extends Component {
+  render() {
+    return (
+      <h1>You chose <span className="badge badge-primary">Services</span></h1>
+    );
+  }
+}
+
+export default ServicesComponent;

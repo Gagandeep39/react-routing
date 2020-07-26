bdaf46742c334c6bb2f6f3baa16ad176edbee4ae
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 17:19:38 +0530

    Added Contact Component

---
+++ src/components/Contact/ContactComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ContactComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          You chose <span class='badge badge-primary'>Contact</span>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default ContactComponent;
